c13733eb38153fa74dedeb36c5009bc6cb1c9c97
i-ice-bear <andydowsen47us@gmail.com>
2023-04-06 17:35:33 +0530

    https://github.com/i-ice-bear/sonichype

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello, world")
+
+class Main:
+    def __init__(self, _age, _name):
+        self.age = _age
+        self.name = _name
+
+    def __print__statement(self):
+        return f"Name : {self.name}" \
+               f"Age : {self.age} "
