45d8ac1b261b08eb96436550d9ac89aa918b0f08
i-ice-bear <andydowsen47us@gmail.com>
2023-04-06 17:38:48 +0530

    Header commit

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ print("hello, world")
 
 class Main:
     def __init__(self, _age, _name):
-        self.age = _age
         self.name = _name
+        self.age = _age
 
     def __print__statement(self):
         return f"Name : {self.name}" \
                f"Age : {self.age} "
+
+    def __str__(self):
+        return self.__print__statement()
+
+
+anshu = Main("Anshu", 12)
+print(anshu.name)
